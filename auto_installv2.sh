d744298be2302192405ab2969d27afc436267d66
So4resAlex <53014800+So4resAlex@users.noreply.github.com>
2023-07-21 21:58:36 -0300

    Add files via upload

---
+++ auto_installv2.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+. funcoes
+
+VALOR=$1 
+NOME_USER=$2
+
+if [ -z "$VALOR" ]; then 
+echo "Especifique o primeiro argumento" 
+exit 1
+else [ -z "$NOME_USER" ]; then
+echo "Especifique o segundo argumento" 
+exit 2
+fi
+case "$VALOR" in
+	all)
+       verifica_user && \
+       instala_curl && \
+       verifica_user && \
+       instala_flatpack	&& \
+       install_oh_my_posh && \
+       instala_configura_vim && \
+       manipula_bashrc
+  ;;
+esac
+exit 0
