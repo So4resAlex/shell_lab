e9181cb1793b02bb54d4195d9fe7331b6dabdb4e
So4resAlex <53014800+So4resAlex@users.noreply.github.com>
2023-07-21 22:19:19 -0300

    Update auto_installv2.sh

--- auto_installv2.sh
+++ auto_installv2.sh
@@ -1,26 +1,36 @@
 #!/bin/sh
 
-./funcoes.sh
+. funcoes.sh
 
-VALOR=$1 
+VALOR=$1
 NOME_USER=$2
 
-if [ -z "$VALOR" ]; then 
-echo "Especifique o primeiro argumento" 
-exit 1
-	if [ -z "$NOME_USER" ]; then
-		echo "Especifique o segundo argumento" 
-exit 2
+if [ -z "$VALOR" ]; then
+  echo "Especifique o primeiro argumento"
+  exit 1
 fi
+
+if [ -z "$NOME_USER" ]; then
+  echo "Especifique o segundo argumento"
+  exit 2
+fi
+
 case "$VALOR" in
-	all)
-       verifica_user && \
-       instala_curl && \
-       verifica_user && \
-       instala_flatpack	&& \
-       install_oh_my_posh && \
-       instala_configura_vim && \
-       manipula_bashrc
-  ;;
+  all)
+    verifica_user && \
+    instala_curl && \
+    verifica_user && \
+    instala_flatpack && \
+    install_oh_my_posh && \
+    instala_configura_vim && \
+    manipula_bashrc
+    ;;
+  *)
+    echo "Opção inválida: $VALOR"
+    exit 3
+    ;;
 esac
+
 exit 0
+
+
